a5381aea6510ec4c88bc323d6a948f84dc3424c4
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-12-29 17:51:48 +0530

    Interfaceee

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title="Safe State Synthsesis",page_icon=":books:")
+    st.header("Create Your own DFA")
+    st.text_input("Shoot Your Question")
+    with st.sidebar:
+        st.subheader("Add The Paper")
+        st.file_uploader("Upload your Paper Here and Click submit")
+        st.button("Submit")
+
+if __name__=='__main__':
+    main()
