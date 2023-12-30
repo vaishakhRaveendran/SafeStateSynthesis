3e16b03806332c1f0bd1ebb06ee51008bb1e8253
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-12-30 17:43:32 +0530

    extract text from the papers

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def get_paper_text(paper_docs):
+    text = ""
+    for paper in paper_docs:
+        paper_reader = PdfReader(paper)
+        for page in paper_reader.pages:
+            text += page.extract_text()
+    return text
 
 def main():
     load_dotenv()
@@ -8,8 +17,12 @@ def main():
     st.text_input("Shoot Your Question")
     with st.sidebar:
         st.subheader("Add The Paper")
-        st.file_uploader("Upload your Paper Here and Click submit")
-        st.button("Submit")
+        paper_docs=st.file_uploader("Upload your Paper Here and Click submit",accept_multiple_files=True)
+        if st.button("Submit"):
+            with st.spinner("Vectorising"):
+                #Get the paper
+                raw_text=get_paper_text(paper_docs)
+
 
 if __name__=='__main__':
     main()
