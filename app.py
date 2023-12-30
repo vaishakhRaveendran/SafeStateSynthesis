942fa4b33acd479fb7fc826fd1dcee169ff7396b
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-12-30 17:27:06 +0530

    adding api keys for langchain

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Safe State Synthsesis",page_icon=":books:")
     st.header("Create Your own DFA")
     st.text_input("Shoot Your Question")
